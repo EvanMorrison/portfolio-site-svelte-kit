f68d7cdff77f3b528981d01b9bc8fb372f1170dc
Evan Morrison <evanmorrison@filevine.com>
2021-12-18 22:01:11 -0700

    Add icons and formatting for Contact section

---
+++ src/lib/data/imgUrls.ts
@@ -0,0 +1,43 @@
+const logoUrls = {
+  ReactJS: '/static/images/logos/reactjs1.jpg',
+  Redux: '/static/images/logos/redux.jpg',
+  Java: '/static/images/logos/java.png',
+  Elixir: '/static/images/logos/elixir.png',
+  JavaScript: '/static/images/logos/js.png',
+  HTML5: '/static/images/logos/html5-wt.png',
+  CSS3: '/static/images/logos/css3-wt.png',
+  Emotion: '/static/images/logos/emotion.png',
+  Nextjs: '/static/images/logos/next-js.jpg',
+  Nodejs: '/static/images/logos/nodejs.png',
+  Webpack: '/static/images/logos/webpack-wt.jpg',
+  express: '/static/images/logos/express-wt.png',
+  MySQL: '/static/images/logos/my_sql.png',
+  MongoDB: '/static/images/logos/mongodb.jpg',
+  PostgreSQL: '/static/images/logos/postgresql.png',
+  AngularJS: '/static/images/logos/angular.png',
+  Sass: '/static/images/logos/sass.jpg',
+  AngularMaterial: '/static/images/logos/angular_materialdesign.jpg',
+  Github: '/static/images/logos/github.png',
+  npm: '/static/images/logos/npm.png',
+  ES6: '/static/images/logos/es6.png',
+  Babel: '/static/images/logos/babel_solid.png',
+  UIRouter: '/static/images/logos/UI_Router.jpg',
+};
+
+const projectUrls = {
+  'Tazworks Instascreen': 'static/images/projects/instascreen-500x300.jpg',
+  'Rich Text Editor': 'static/images/projects/draft-js-500-300.jpg',
+  'Multiselect Demo': 'static/images/projects/multiselect-500x300.jpg',
+  'Lymphatic Therapy': 'static/images/projects/dynamiclymphatics-500x300.jpg',
+  'Therapy Business': 'static/images/projects/mt4wRomano-500x300.jpg',
+  'Moshero CPA Business': 'static/images/projects/mosheroCpa-500x300.jpg',
+  'Portfolio Site': 'static/images/projects/portfolioSite-500x300.jpg',
+  'Real-T': 'static/images/projects/realt-home-1080x720.jpg',
+};
+
+const heroUrls = [
+  'static/images/heros/laptop-2434393_1280.jpg',
+  'static/images/heros/macbook-from-back-1080x720.jpg',
+];
+
+export { logoUrls, projectUrls, heroUrls };
